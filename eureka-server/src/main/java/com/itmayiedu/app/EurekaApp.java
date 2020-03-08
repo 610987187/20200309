533b25e095df57836ddf262964f4d50c96489dda
yuhuijuan1 <yuhuijuan1@example.com>
2020-03-09 02:48:28 +0800

    11

---
+++ eureka-server/src/main/java/com/itmayiedu/app/EurekaApp.java
@@ -0,0 +1,17 @@
+package com.itmayiedu.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApp.class, args);
+		System.out.print("11111115555511111133333");
+		System.out.print("1--");
+	}
+
+}
